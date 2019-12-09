585c0c95d07bdb83eabd06d7c1feac9835f2c6a9
PickPlugins <32812881+pickplugins@users.noreply.github.com>
2019-12-09 14:43:40 +0600

    u

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -15,37 +15,45 @@ function bekarcombd_jobs_search_display($atts, $content = null ) {
 
     $bekar_keyword = '';
     $bekar_location = '';
+    if ( get_query_var('paged') ) {$paged = get_query_var('paged');}
+    elseif ( get_query_var('page') ) {$paged = get_query_var('page');}
+    else {$paged = 1;}
+
+    $per_page = isset($_GET['per_page']) ? sanitize_text_field($_GET['per_page']) : 10;
+
+    //echo '<pre>'.var_export($paged, true).'</pre>';
+
 
     if(! isset( $_POST['bekarcombd_nonce'] ) || ! wp_verify_nonce( $_POST['bekarcombd_nonce'], 'bekarcombd_nonce' ) ){
 
         $bekar_keyword = isset($_GET['bekar_keyword']) ? sanitize_text_field($_GET['bekar_keyword']) : '';
         $bekar_location = isset($_GET['bekar_location']) ? sanitize_text_field($_GET['bekar_location']) : '';
 
-        $api_url = 'https://bekar.com.bd/job-search-api/?per_page=5&keywords='.urlencode($bekar_keyword);
+        $api_url = 'https://bekar.com.bd/job-search-api/?per_page='.$per_page.'&paged='.$paged.'&keywords='.urlencode($bekar_keyword);
 
     }else{
         $api_url = 'https://bekar.com.bd/job-search-api/?per_page=10';
     }
 
-    if ( get_query_var('paged') ) {$paged = get_query_var('paged');}
-    elseif ( get_query_var('page') ) {$paged = get_query_var('page');}
-    else {$paged = 1;}
 
-    $per_page = isset($_GET['per_page']) ? sanitize_text_field($_GET['per_page']) : 5;
+
 
     $response = wp_remote_get( $api_url );
     $body = wp_remote_retrieve_body( $response );
     $response_data =  json_decode($body);
 
-    $jobs = $response_data->jobs;
-    $found_posts = $response_data->found_posts;
+    $jobs = isset($response_data->jobs) ? $response_data->jobs : array();
+    $found_posts = isset($response_data->found_posts) ? $response_data->found_posts : '';
 
-    echo '<pre>'.var_export($found_posts, true).'</pre>';
+    //echo '<pre>'.var_export($_SERVER, true).'</pre>';
+
+
+
     ?>
 
     <div class="bekarcombd-jobs">
 
-        <form method="get" action="">
+        <form method="get" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
             <input placeholder="Keyword" type="search" value="<?php echo $bekar_keyword; ?>" name="bekar_keyword">
             <input placeholder="Location" type="search" value="<?php echo $bekar_location; ?>" name="bekar_location">
 
@@ -56,13 +64,15 @@ function bekarcombd_jobs_search_display($atts, $content = null ) {
         <ul class="job-list">
             <?php
 
-            if(!empty($jobs))
+            if(!empty($jobs)):
             foreach ($jobs as $job){
                 $title = $job->title;
                 $url = $job->url;
                 $publish_date = $job->publish_date;
                 $expire_date = $job->expire_date;
                 $company_name = $job->company_name;
+                $import_source = $job->import_source;
+
 
                 $publish_date = strtotime($publish_date);
 
@@ -84,10 +94,31 @@ function bekarcombd_jobs_search_display($atts, $content = null ) {
                         <?php if(!empty($company_name)): ?>
                         <span>Company: <?php echo $company_name; ?></span>
                         <?php endif; ?>
+
+                        <?php if(!empty($import_source)): ?>
+                            <span>Source: <?php echo $import_source; ?></span>
+                        <?php endif; ?>
+
                     </div>
                 </li>
                 <?php
             }
+            else:
+                ?>
+            <li class="job">
+                <div class="job-title">Sorry! there is a server error, please try again.</div>
+            </li>
+                <?php
+            endif;
+
+
+
+
+            ?>
+        </ul>
+
+        <div class="pagination">
+            <?php
 
             $big = $found_posts; // need an unlikely integer
             echo paginate_links( array(
@@ -97,9 +128,10 @@ function bekarcombd_jobs_search_display($atts, $content = null ) {
                 'total' => $per_page
             ) );
 
-
             ?>
-        </ul>
+        </div>
+
+
     </div>
 
 
@@ -128,8 +160,26 @@ function bekarcombd_jobs_search_display($atts, $content = null ) {
         }
 
         .bekarcombd-jobs .job-meta{
-            font-size: 12px;
+
         }
+
+        .bekarcombd-jobs .job-meta span{
+            font-size: 12px;
+            display: inline-block;
+            margin: 0 10px 0 0;
+            padding: 0 10px 0 0;
+        }
+
+        .bekarcombd-jobs .pagination{}
+        .bekarcombd-jobs .pagination .page-numbers{
+            padding: 5px 15px;
+            background: #ddd;
+            margin: 0 4px;
+        }
+        .bekarcombd-jobs .pagination .page-numbers.current{
+            background: #b5b5b5;
+        }
+
 
 
     </style>
